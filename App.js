cf40b5a6e2dce148df77effb6c3a6e847e3e59f3
Mauricio Mercado <mau@maumercado.com>
2023-12-01 13:39:08 -0800

    Add logout

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { StatusBar } from 'expo-status-bar'
 
 import AuthContextProvider, { AuthContext } from './store/auth-context'
 
+import IconButton from './components/ui/IconButton'
+
 import LoginScreen from './screens/LoginScreen'
 import SignupScreen from './screens/SignupScreen'
 import WelcomeScreen from './screens/WelcomeScreen'
@@ -28,6 +30,7 @@ function AuthStack () {
 }
 
 function AuthenticatedStack () {
+  const authCtx = useContext(AuthContext)
   return (
     <Stack.Navigator
       screenOptions={{
@@ -36,7 +39,11 @@ function AuthenticatedStack () {
         contentStyle: { backgroundColor: Colors.primary100 }
       }}
     >
-      <Stack.Screen name='Welcome' component={WelcomeScreen} />
+      <Stack.Screen
+        name='Welcome' component={WelcomeScreen} options={{
+          headerRight: ({ tintColor }) => <IconButton icon='exit' color={tintColor} size='24' onPress={authCtx.logout} />
+        }}
+      />
     </Stack.Navigator>
   )
 }
